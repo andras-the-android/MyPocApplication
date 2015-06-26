a8a4181708c5bcea02158ed299d5b3bc0a057f09
Andras Nemeth <andras_nemeth@epam.com>
2015-06-26 18:21:03 +0200

    notification normalization

--- app/src/main/java/com/example/andras/myapplication/MainActivity.java
+++ app/src/main/java/com/example/andras/myapplication/MainActivity.java
@@ -1,14 +1,8 @@
 package com.example.andras.myapplication;
 
 import android.app.Activity;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,79 +12,10 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    public static final int mId = 666;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //setupNotifications();
-
-    }
-
-
-    private void setupNotifications() {
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cannond);
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
-                        //.setSmallIcon(R.drawable.icon_blue_86x86_stride)
-                        .setContentTitle("My notification")
-                        .setContentText("Hello World!")
-                        .setLargeIcon(bitmap)
-                        .setAutoCancel(true);
-        //.setProgress(0, 0, true);
-
-        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
-        String[] events = new String[6];
-// Sets a title for the Inbox in expanded layout
-        inboxStyle.setBigContentTitle("Event tracker details:");
-        inboxStyle.setSummaryText("This is the summary. Enjoy!");
-
-// Moves events into the expanded layout
-        for (int i=0; i < 5; i++) {
-
-            inboxStyle.addLine(i + ": sdvoskdvpovw");
-        }
-
-        NotificationCompat.BigPictureStyle notiStyle = new
-                NotificationCompat.BigPictureStyle();
-        notiStyle.setBigContentTitle("Big Picture Expanded");
-        notiStyle.setSummaryText("Nice big picture.");
-        //notiStyle.bigText("Big text");
-        notiStyle.setBigContentTitle("big content tite");
-        notiStyle.setSummaryText("Summary text");
-
-
-// Add the big picture to the style.
-        notiStyle.bigPicture(bitmap);
-
-
-// Moves the expanded layout object into the notification object.
-        mBuilder.setStyle(inboxStyle);
-// Creates an explicit intent for an Activity in your app
-        Intent resultIntent = new Intent(this, MainActivity.class);
-
-// The stack builder object will contain an artificial back stack for the
-// started Activity.
-// This ensures that navigating backward from the Activity leads out of
-// your application to the Home screen.
-        //TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-// Adds the back stack for the Intent (but not the Intent itself)
-        //stackBuilder.addParentStack(ResultActivity.class);
-// Adds the Intent that starts the Activity to the top of the stack
-        //stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_CANCEL_CURRENT );
-//                stackBuilder.getPendingIntent(
-//                        0,
-//                        PendingIntent.FLAG_UPDATE_CURRENT
-//                );
-        mBuilder.setContentIntent(resultPendingIntent);
-        mBuilder.addAction(R.drawable.ic_launcher, "First", resultPendingIntent);
-        mBuilder.addAction(R.drawable.ic_launcher, "Second", resultPendingIntent);
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-// mId allows you to update the notification later on.
-//        mNotificationManager.notify(mId, mBuilder.build());
     }
 
     @Override
@@ -137,5 +62,9 @@ public class MainActivity extends Activity {
 
     public void onDragNDropButtonClick(View view) {
         startActivity(new Intent(this, DragDropActivity.class));
+    }
+
+    public void onNotificationButtonClick(View view) {
+        startActivity(new Intent(this, NotificationActivity.class));
     }
 }
---
+++ app/src/main/java/com/example/andras/myapplication/NotificationActivity.java
@@ -0,0 +1,91 @@
+package com.example.andras.myapplication;
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
+public class NotificationActivity extends Activity {
+
+    public static final int mId = 666;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+        setupNotifications();
+    }
+
+    private void setupNotifications() {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cannond);
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        //alternatively you can add stack to pending intent
+//        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+//        stackBuilder.addParentStack(ResultActivity.class);
+//        stackBuilder.addNextIntent(resultIntent);
+//        stackBuilder.getPendingIntent(
+//                0,
+//                PendingIntent.FLAG_UPDATE_CURRENT
+//        );
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_CANCEL_CURRENT );
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        //Manadatory items
+                        //------
+                        .setSmallIcon(R.drawable.icon_blue_86x86_stride)
+                        .setContentTitle("Content title")
+                        .setContentText("Content text")
+                        //------
+                        .setLargeIcon(bitmap)
+                        //dismiss notification atrer the user tapped on it
+                        .setAutoCancel(true)
+                        //this generates an indeterminate progress bar
+                        //.setProgress(0, 0, true)
+                        ;
+        //alternative configuration way
+        mBuilder.setContentIntent(resultPendingIntent);
+        mBuilder.addAction(R.drawable.ic_launcher, "First", resultPendingIntent);
+        mBuilder.addAction(R.drawable.ic_launcher, "Second", resultPendingIntent);
+
+        //inbox style--------------------------------------------------------------------------------
+
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        String[] events = new String[6];
+        inboxStyle.setBigContentTitle("Big content title");
+        inboxStyle.setSummaryText("Summary text");
+
+        // Moves events into the expanded layout
+        for (int i=0; i < 5; i++) {
+            inboxStyle.addLine(i + ": inbox style line");
+        }
+        mBuilder.setStyle(inboxStyle);
+
+        //big picture style---------------------------------------------------------------------------
+
+        NotificationCompat.BigPictureStyle bigPictureStyle = new NotificationCompat.BigPictureStyle();
+        bigPictureStyle.setBigContentTitle("Big content title");
+        bigPictureStyle.setSummaryText("Summary text");
+        bigPictureStyle.bigPicture(bitmap);
+        //mBuilder.setStyle(bigPictureStyle);
+
+        //big text style-------------------------------------------------------------------------------
+
+        NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
+        bigTextStyle.bigText("Big text");
+        bigTextStyle.setBigContentTitle("Big content tite");
+        bigTextStyle.setSummaryText("Summary text");
+        //mBuilder.setStyle(bigTextStyle);
+
+        //show the notification
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(mId, mBuilder.build());
+    }
+}
