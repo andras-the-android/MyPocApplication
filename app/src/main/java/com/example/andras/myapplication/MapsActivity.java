1639382e607fee6fc411d74ab4457269b410fa2d
Andras Nemeth <andras_nemeth@epam.com>
2015-07-09 22:10:43 +0200

    maps #2

--- app/src/main/java/com/example/andras/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/andras/myapplication/MapsActivity.java
@@ -1,17 +1,24 @@
 package com.example.andras.myapplication;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends Activity {
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    private MapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +50,14 @@ public class MapsActivity extends Activity {
      */
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
+        // GoogleMap might be null if Google Play services APK is not available.
+        if (mapFragment == null) {
             // Try to obtain the map from the SupportMapFragment.
-            mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
+            GoogleMapOptions mapOptions = new GoogleMapOptions();
+            mapFragment = MapFragment.newInstance(mapOptions);
+            getFragmentManager().beginTransaction().add(R.id.map, mapFragment).commit();
+            mapFragment.getMapAsync(new MyMapReadyCallback());
+
         }
     }
 
@@ -58,9 +65,30 @@ public class MapsActivity extends Activity {
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
      * just add a marker near Africa.
      * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(47.485447d, 19.070975d)).title("Marker"));
+        GoogleMap map = mapFragment.getMap();
+        LatLng latLng = new LatLng(47.485447d, 19.070975d);
+        map.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+        map.setIndoorEnabled(false);
+        map.setMyLocationEnabled(true);
+        //Location location = map.getMyLocation();
+//        CameraPosition position = new CameraPosition.Builder().target(new LatLng(location.getLatitude(),location.getLongitude())).build();
+        CameraPosition position = new CameraPosition.Builder().target(latLng).zoom(20).build();
+
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(position));
+    }
+
+    private class MyMapReadyCallback implements OnMapReadyCallback {
+
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            ((TextView)findViewById(R.id.text_view)).setText("Map ready!");
+            setUpMap();
+        }
+    }
+
+    private void doAutoFit() {
+
     }
 }
