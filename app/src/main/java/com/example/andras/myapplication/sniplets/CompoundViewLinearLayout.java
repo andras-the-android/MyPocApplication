95e8494e6d5df761e0a23418bf9f0a5850f667c1
Andras Nemeth <guni82hu@gmail.com>
2016-12-22 13:38:15 +0100

    coumpound view template

---
+++ app/src/main/java/com/example/andras/myapplication/sniplets/CompoundViewLinearLayout.java
@@ -0,0 +1,49 @@
+package com.example.andras.myapplication.sniplets;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.andras.myapplication.R;
+
+/**
+ * Created by Andras_Nemeth on 2016. 12. 22..
+ */
+
+public class CompoundViewLinearLayout extends LinearLayout {
+
+    public CompoundViewLinearLayout(Context context) {
+        super(context);
+        initView(context, false);
+    }
+
+    public CompoundViewLinearLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initView(context, true);
+    }
+
+    public CompoundViewLinearLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(context, true);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CompoundViewLinearLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        initView(context, true);
+    }
+
+    private void initView(Context context, boolean hasAttributes) {
+        View view = LayoutInflater.from(context).inflate(R.layout.view_compound, this, true);
+        if (!hasAttributes) {
+            setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        }
+        setOrientation(VERTICAL);
+    }
+}
